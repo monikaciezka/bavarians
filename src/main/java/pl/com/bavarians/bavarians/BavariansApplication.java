9a5aa51ca223272b22914f9c7dfca059ba145888
mca-dev <magdalena.ciezka@gmail.com>
2022-01-16 19:28:29 +0100

    Initial commit

---
+++ src/main/java/pl/com/bavarians/bavarians/BavariansApplication.java
@@ -0,0 +1,13 @@
+package pl.com.bavarians.bavarians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BavariansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BavariansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/com/bavarians/bavarians/BavariansApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.bavarians.bavarians;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BavariansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
